8bbf5c0c76dc449ef46213ecdd42969f89b9f6e5
Richard Hogoboom <rhogoboom@gmail.com>
2023-11-05 18:17:40 -0500

    feat: cells change color on mouseover

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ populateContainer();
 
 function populateContainer() {
     const container = document.querySelector('.container');
-    let n = 16;
+    let n = 50;
 
     for (let i = 0; i < n; i++) {
         createRow(container, n);
@@ -13,9 +13,20 @@ function createRow(container, size) {
     const newRow = document.createElement('div');
     newRow.classList.add('etch-row');
     for (let j = 0; j < size; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('etch-cell');
-        newRow.appendChild(newDiv);
+        
+        newRow.appendChild(createCell());
     }
     container.appendChild(newRow);
+}
+
+function hoverBox(e) {
+    e.target.classList.add('hovered')
+    console.dir(e);
+}
+
+function createCell() {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('etch-cell');
+    newDiv.addEventListener('mouseenter', hoverBox);
+    return newDiv
 }
