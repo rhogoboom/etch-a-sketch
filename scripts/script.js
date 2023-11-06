2df81205218b074c3e0baf1ed3213248e7f1b1d9
Richard Hogoboom <rhogoboom@gmail.com>
2023-11-06 08:20:20 -0500

    feat: Choose between two color options

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,24 @@ const resolutionButton = document.querySelector('.resolution-button');
 
 resolutionButton.addEventListener('click', handleResolutionClick);
 
+const optionsButtons = [...document.querySelectorAll('.options button')];
+
+optionsButtons.forEach(btn => {
+    btn.addEventListener('click', handleOptionClick);
+})
+
 function populateContainer(size=16) {
     const container = document.querySelector('.container');
     const newRows = []
 
     for (let i = 0; i < size; i++) {
-        const newRow = createRow(container, size);
+        const newRow = createRow(size);
         newRows.push(newRow);
     }
     container.replaceChildren(...newRows)
 }
 
-function createRow(container, size) {
+function createRow(size) {
     const newRow = document.createElement('div');
     newRow.classList.add('etch-row');
     for (let j = 0; j < size; j++) {
@@ -28,7 +34,15 @@ function createRow(container, size) {
 }
 
 function hoverBox(e) {
-    e.target.classList.add('hovered')
+    const options = [...document.querySelectorAll('.options button')];
+    let newOption;
+    options.forEach(option => {
+        if (option.classList.contains('current-option')) {
+            newOption = option.id;
+        }
+    })
+    
+    e.target.classList.add(newOption)
 }
 
 function createCell() {
@@ -52,3 +66,27 @@ function handleResolutionClick() {
     populateContainer(getNewSize());
 }
 
+function handleOptionClick(e) {
+    optionsButtons.forEach(btn => {
+        if (btn.classList.contains('current-option')) {
+            btn.classList.remove('current-option');
+        }
+        
+        }
+    )
+    e.target.classList.add('current-option');
+ 
+
+
+    
+ 
+}
+
+function updateListeners() {
+    const cells = [...document.querySelectorAll('.etch-cell')];
+
+    cells.forEach(cell => {
+        cell.removeEventListener('click', hoverBox);
+        cell.addEventListener('click', hoverBox);
+    })
+}
